b57f703ec9d1251a9bedb2e067aff25e0eebfd56
breigner11 <breigner11@outlook.com>
2018-03-21 21:38:21 -0400

    initial commmit

---
+++ app/src/main/java/com/example/brr002/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.example.brr002.geoquiz;
+
+/**
+ * Created by BRR002 on 3/19/2018.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+
+    }
+}
